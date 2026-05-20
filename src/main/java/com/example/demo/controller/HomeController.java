1961f3617d0c381fe6c5c8e6062dfdc120aa198b
your name <your@example.com>
2026-05-20 12:26:55 +0900

    Fix HomeController use sendRedirect

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
-    public String home() {
-        return "redirect:/register.html";
+    @GetMapping("/")
+    public void home(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/register.html");
     }
 }
